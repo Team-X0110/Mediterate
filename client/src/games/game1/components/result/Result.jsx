2079d78e7272d4125ea4e86f32b36176e21d5d41
Nikhil-2x <nikhilyadav101513@gmail.com>
2025-08-30 12:14:18 +0530

    fix-game1-result-card

--- client/src/games/game1/components/result/Result.jsx
+++ client/src/games/game1/components/result/Result.jsx
@@ -1,105 +1,7 @@
-// import { useNavigate } from "react-router-dom";
-// import useStore from "../../stores/game";
-// import { useState } from "react";
-
-// export function Result() {
-//   const status = useStore((state) => state.status);
-//   const score = useStore((state) => state.score);
-//   const reset = useStore((state) => state.reset);
-//   const navigate = useNavigate();
-
-//   const [showVideo, setShowVideo] = useState(false);
-
-//   if (status === "running") return null;
-
-//   return (
-//     <div id="result-container">
-//       {showVideo ? (
-//         //  Fullscreen Video Modal
-//         <div
-//           style={{
-//             position: "fixed",
-//             inset: 0,
-//             background: "rgba(0, 0, 0, 0.9)",
-//             display: "flex",
-//             justifyContent: "center",
-//             alignItems: "center",
-//             zIndex: 1000,
-//           }}
-//         >
-//           <video
-//             style={{
-//               maxWidth: "90%",
-//               maxHeight: "90%",
-//               borderRadius: "12px",
-//               boxShadow: "0 0 20px rgba(0,0,0,0.8)",
-//             }}
-//             // autoPlay
-//             controls
-//             controlsList="download"
-//             onEnded={() => {
-//               setShowVideo(false);
-//               navigate("/game/1");
-//             }}
-//           >
-//             <source src="/assets/videos/cat.mp4" type="video/mp4" />
-//             Your browser does not support the video tag.
-//           </video>
-
-//           {/*  Close Button */}
-//           <button
-//             onClick={() => setShowVideo(false)}
-//             style={{
-//               position: "absolute",
-//               top: "20px",
-//               right: "30px",
-//               fontSize: "2rem",
-//               background: "transparent",
-//               border: "none",
-//               color: "white",
-//               cursor: "pointer",
-//             }}
-//           >
-//             ✖
-//           </button>
-//         </div>
-//       ) : (
-//         //  Normal Game Over Screen
-//         <div id="result">
-//           <h1>Game Over</h1>
-//           <p>Your score: {score}</p>
-//           <div id="button-container">
-//             <div id="first-btn">
-//               <button onClick={reset}>Retry</button>
-//               <button
-//                 onClick={() => {
-//                   navigate(`/`);
-//                 }}
-//               >
-//                 Home
-//               </button>
-//             </div>
-
-//             <div id="second-btn">
-//               <button
-//                 onClick={() => {
-//                   navigate(`/quiz`);
-//                 }}
-//               >
-//                 Take Quiz
-//               </button>
-//               <button onClick={() => setShowVideo(true)}> Watch Video</button>
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
 import { useNavigate } from "react-router-dom";
 import useStore from "../../stores/game";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import VariableProximity from "../../../../blocks/TextAnimations/VariableProximity/VariableProximity";
 
 export function Result() {
   const status = useStore((state) => state.status);
@@ -109,6 +11,8 @@ export function Result() {
   const markQuizCompleted = useStore((state) => state.markQuizCompleted);
   const videoWatched = useStore((state) => state.videoWatched);
   const quizCompleted = useStore((state) => state.quizCompleted);
+
+  const ref = useRef();
 
   const navigate = useNavigate();
   const [showVideo, setShowVideo] = useState(false);
@@ -140,7 +44,7 @@ export function Result() {
             controls
             onEnded={() => {
               setShowVideo(false);
-              markVideoWatched(); // ✅ mark as completed
+              markVideoWatched(); //  mark as completed
               navigate("/game/1");
             }}
           >
@@ -165,11 +69,22 @@ export function Result() {
           </button>
         </div>
       ) : (
-        <div id="result">
-          <h1>Game Over</h1>
+        <div id="result" ref={ref}>
+          <h1>
+            <VariableProximity
+              label={"Game Over"}
+              className={"variable-proximity-demo"}
+              fromFontVariationSettings="'wght' 400, 'opsz' 9"
+              toFontVariationSettings="'wght' 4000, 'opsz' 40"
+              containerRef={ref}
+              radius={100}
+              falloff="gaussian"
+            />
+          </h1>
+
           <p>Your score: {score}</p>
           <div id="button-container">
-            {/* ✅ Retry only if BOTH tasks are done */}
+            {/* Retry only if BOTH tasks are done */}
             <div id="first-btn">
               {videoWatched && quizCompleted && (
                 <button className="btn-gradient-text" onClick={reset}>
@@ -189,7 +104,7 @@ export function Result() {
               <button
                 className="btn-gradient-text"
                 onClick={() => {
-                  markQuizCompleted(); // ✅ mark quiz done
+                  markQuizCompleted(); // mark quiz done
                   navigate(`/game/1`);
                 }}
               >
